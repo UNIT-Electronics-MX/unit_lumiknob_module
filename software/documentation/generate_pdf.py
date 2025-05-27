601af3908cd5bb0e2905a57493e7a114c4c065e1
Cesarbautista10 <cesar.bautista@uelectronis.com>
2025-05-27 10:13:32 -0600

    chore: update size image

--- software/documentation/generate_pdf.py
+++ software/documentation/generate_pdf.py
@@ -89,7 +89,7 @@ def format_images_with_titles(content):
             f"\\section*{{{title}}}\n"
             "\\vspace{1em}\n"
             "\\begin{center}\n"
-            f"\\includegraphics[width=0.75\\textwidth,keepaspectratio]{{{path}}}\n"
+            f"\\includegraphics[width=0.85\\textwidth,keepaspectratio]{{{path}}}\n"
             "\\end{center}\n"
         )
     return code
